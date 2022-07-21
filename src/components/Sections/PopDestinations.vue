bda08954deaac3ea05f82ec55ee812a12aeb826e
abolfazl-hue <abolfazljdst@gmail.com>
2022-07-21 11:08:39 +0430

    add title of popular section

--- src/components/Sections/PopDestinations.vue
+++ src/components/Sections/PopDestinations.vue
@@ -1,12 +1,15 @@
 <script lang="ts" setup>
 import uniteds from '@/stores/uniteds'
+import defaultData from '@/stores/data'
 </script>
 
 <template>
     <section class="text-center bg-light-900 mt-87px pt-100px pb-135px block">
         <div class="container px-5 mx-auto">
             <div class="w-full text-center mb-57px">
-                <h2 class="text-green-800"></h2>
+                <h2 class="text-green-800">
+                    {{ defaultData.sections.popular.title }}
+                </h2>
             </div>
             <div class="grid grid-cols-3 gap-8 px-120px">
                 <United
